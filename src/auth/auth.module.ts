5c4fbf3cda0b725ebbf500194fd3f03a7f868b3c
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2025-07-26 23:36:52 +0500

    auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,5 +19,6 @@ import { JwtModule } from '@nestjs/jwt';
   ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService, JwtModule, UserModule],
 })
 export class AuthModule {}
